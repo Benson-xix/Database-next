be0c9b87461fcfa5d6ff2704da957236807cd538
uzoefuna benson dominic <uzoefunam.benson@gmail.com>
2023-10-29 21:31:20 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
-import Head from "next/head";
-import ManhwaSearch from "./components/ManhwaSearch";
+"use client"
+
+import React, { useState, useEffect } from 'react';
+import Head from 'next/head';
+import ManhwaSearch from './components/ManhwaSearch';
+import LoadingSpinner from './components/LoadingSpinner ';
+
 
 const Page = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      // Simulating the loading process
+      setIsLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="bg-black min-h-screen">
       <Head>
@@ -9,11 +25,14 @@ const Page = () => {
         <meta name="description" content="A simple Manhwa Database for users" />
       </Head>
       <main className="container mx-auto py-18 px-4 flex flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold mb-8 text-white ">
-          Manhwa Database
-        </h1>
-
-        <ManhwaSearch/>
+        {isLoading ? (
+          <LoadingSpinner />
+        ) : (
+          <>
+            <h1 className="text-4xl font-bold mb-8 text-white ">Manhwa Database</h1>
+            <ManhwaSearch />
+          </>
+        )}
       </main>
     </div>
   );
