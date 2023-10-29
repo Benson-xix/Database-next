539339bb2f4b89018f9276de66a98f909a580590
uzoefuna benson dominic <uzoefunam.benson@gmail.com>
2023-10-29 22:11:32 +0100

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,11 @@
-"use client"
 
-import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 import ManhwaSearch from './components/ManhwaSearch';
-import LoadingSpinner from './components/LoadingSpinner ';
+
 
 
 const Page = () => {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      // Simulating the loading process
-      setIsLoading(false);
-    }, 3000);
-
-    return () => clearTimeout(timer);
-  }, []);
+  
 
   return (
     <div className="bg-black min-h-screen">
@@ -25,14 +14,8 @@ const Page = () => {
         <meta name="description" content="A simple Manhwa Database for users" />
       </Head>
       <main className="container mx-auto py-18 px-4 flex flex-col items-center justify-center">
-        {isLoading ? (
-          <LoadingSpinner />
-        ) : (
-          <>
-            <h1 className="text-4xl font-bold mb-8 text-white ">Manhwa Database</h1>
+      <h1 className="text-4xl font-bold mb-8 text-white ">Manhwa Database</h1>
             <ManhwaSearch />
-          </>
-        )}
       </main>
     </div>
   );
