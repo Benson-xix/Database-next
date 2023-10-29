0283205d65cece1df0006f80214c69ae297a1037
uzoefuna benson dominic <uzoefunam.benson@gmail.com>
2023-10-29 21:47:27 +0100

    Update LoadingSpinner .tsx

--- app/components/LoadingSpinner .tsx
+++ app/components/LoadingSpinner .tsx
@@ -6,7 +6,7 @@ const LoadingSpinner  = () => {
    
   return (
     <div className="flex items-center justify-center h-screen">
-    <div className="w-16 h-16 border-t-4 border-blue-500 rounded-full animate-spin"></div>
+    <div className="w-16 h-16 border-t-4 border-red-500 rounded-full animate-spin"></div>
   </div>
   )
 }
