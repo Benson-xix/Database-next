1ad88e9c55b4e3459c30ddc6239b7a597ed81036
uzoefuna benson dominic <uzoefunam.benson@gmail.com>
2023-11-06 15:52:58 +0100

    Update ManhwaSearch.tsx

--- app/components/ManhwaSearch.tsx
+++ app/components/ManhwaSearch.tsx
@@ -42,7 +42,7 @@ import LoadingSpinner from './LoadingSpinner ';
         <LoadingSpinner />
       ) : (
         <>
-          <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 mt-8">
+          <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 p-4 md:p-0 mt-8">
             {manhwa &&
               manhwa.map((manhwa: any, index: number) => (
                 <ManhwaCard
